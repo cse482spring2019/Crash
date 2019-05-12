7666bbee063e96ed67af447c3d3fd7cb22361628
Nicole Riley <nriley16@uw.edu>
2019-05-12 14:03:13 -0700

    splash screen

--- app/screens/SplashScreen.js
+++ app/screens/SplashScreen.js
@@ -11,13 +11,14 @@ import {
 } from 'react-native';
 import { WebBrowser } from 'expo';
 
+//Makes the default splash screen for navigating to the initial stop screen. 
 export default class SplashScreen extends React.Component {
     render() {
         return (
             <TouchableWithoutFeedback onPress={() => this.props.navigation.navigate('InitialStop')}>
-                <View styles={styles.container}>
-                    <Text>
-                        Tap Anywhere to Begin 
+                <View style={styles.container}>
+                    <Text style={styles.titleText}>
+                        TAP ANYWHERE TO BEGIN
                     </Text>
                 </View>
             </TouchableWithoutFeedback>
@@ -26,90 +27,18 @@ export default class SplashScreen extends React.Component {
 }
 
 const styles = StyleSheet.create({
+    colorBlue: {
+      backgroundColor: '#4A78BD'
+    }, 
     container: {
       flex: 1,
-      backgroundColor: '#fff',
-    },
-    developmentModeText: {
-      marginBottom: 20,
-      color: 'rgba(0,0,0,0.4)',
-      fontSize: 14,
-      lineHeight: 19,
-      textAlign: 'center',
-    },
-    contentContainer: {
-      paddingTop: 30,
-    },
-    welcomeContainer: {
+      justifyContent: 'center',
       alignItems: 'center',
-      marginTop: 10,
-      marginBottom: 20,
+      backgroundColor: '#4A78BD'
     },
-    welcomeImage: {
-      width: 100,
-      height: 80,
-      resizeMode: 'contain',
-      marginTop: 3,
-      marginLeft: -10,
-    },
-    getStartedContainer: {
-      alignItems: 'center',
-      marginHorizontal: 50,
-    },
-    homeScreenFilename: {
-      marginVertical: 7,
-    },
-    codeHighlightText: {
-      color: 'rgba(96,100,109, 0.8)',
-    },
-    codeHighlightContainer: {
-      backgroundColor: 'rgba(0,0,0,0.05)',
-      borderRadius: 3,
-      paddingHorizontal: 4,
-    },
-    getStartedText: {
-      fontSize: 17,
-      color: 'rgba(96,100,109, 1)',
-      lineHeight: 24,
-      textAlign: 'center',
-    },
-    tabBarInfoContainer: {
-      position: 'absolute',
-      bottom: 0,
-      left: 0,
-      right: 0,
-      ...Platform.select({
-        ios: {
-          shadowColor: 'black',
-          shadowOffset: { height: -3 },
-          shadowOpacity: 0.1,
-          shadowRadius: 3,
-        },
-        android: {
-          elevation: 20,
-        },
-      }),
-      alignItems: 'center',
-      backgroundColor: '#fbfbfb',
-      paddingVertical: 20,
-    },
-    tabBarInfoText: {
-      fontSize: 17,
-      color: 'rgba(96,100,109, 1)',
-      textAlign: 'center',
-    },
-    navigationFilename: {
-      marginTop: 5,
-    },
-    helpContainer: {
-      marginTop: 15,
-      alignItems: 'center',
-    },
-    helpLink: {
-      paddingVertical: 15,
-    },
-    helpLinkText: {
-      fontSize: 14,
-      color: '#2e78b7',
+    titleText: {
+        fontSize: 30, 
+        fontWeight: 'bold',
+        color: 'white'
     },
   });
