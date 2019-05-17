d71e5c77e0519ddc4634b9946c4fcfe97a7ae828
OfekI <ofekinbar@hotmail.com>
2019-05-16 23:46:27 -0700

    Nicolekrileystopcountdown (#6)
    * starting on stops left screen
    
    * adding as a screen in navigation
    
    * small change
    
    * progress on screens
    
    * fix small bugs

--- app/navigation/MainStackNavigator.js
+++ app/navigation/MainStackNavigator.js
@@ -7,10 +7,12 @@ import InitialStopSelectScreen from '../screens/InitialStopSelectScreen';
 import FinalStopSelectScreen from '../screens/FinalStopSelectScreen';
 import WaitForBusScreen from '../screens/WaitForBusScreen';
 import BusNumberDisplayScreen from '../screens/BusNumberDisplayScreen';
-import { Routes, Stops, Location, Trip } from '../containers';
+import StopsLeftDestinationScreen from '../screens/StopsLeft';
+import { Routes, Stops, Location, Trip, Preferences } from '../containers';
 
 const WrappedWaitForBusScreen = Routes(Stops(Trip(WaitForBusScreen)));
 const WrappedBusNumberDisplayScreen = Routes(Stops(Trip(BusNumberDisplayScreen)));
+const WrappedStopsLeftDestinationScreen = Preferences(Routes(Stops(Trip(StopsLeftDestinationScreen))));
 
 export default createStackNavigator({
   Splash: SplashScreen,
@@ -20,5 +22,5 @@ export default createStackNavigator({
   FinalStopSelect: Stops(FinalStopSelectScreen),
   WaitForBus: props => <WrappedWaitForBusScreen {...props} tripKey="busNumberDisplay" />,
   DisplayBus: props => <WrappedBusNumberDisplayScreen {...props} tripKey="busNumberDisplay" />,
-  Home: HomeScreen,
+  StopsLeft: props => <WrappedStopsLeftDestinationScreen {...props} tripKey="stopsLeft" />,
 }, { headerMode: 'none', initialRouteName: 'Splash' });
